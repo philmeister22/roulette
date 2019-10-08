ae21b843eedf46b315e5665346fdbf07bdf6a35b
Luca <lukaarma@gmail.com>
2019-10-08 19:14:04 +0200

    L: fixato il ciclo for

--- roulette_filippo.c
+++ roulette_filippo.c
@@ -3,29 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct puntata{// struct contenente la coppia valore/tipo di puntata
+typedef struct puntata{// struct contenente la coppia valore/tipo di puntata
     char tipo;
     int valore;
 	struct puntata *succ;
-};
-typedef struct puntata *punt;
+} punt; /* L: Una typedef è migliore se fatta così, più leggibile e puoi
+			  vedere immediatamente la sua struct*/
 
 void n_puntata(punt *l);
 int  main() {
-  punt np = NULL;
+  punt *np; /*L: corretta la definizione di np, cosa sgnifica?*/
   char sc;
   int dsp=10000;
   char gc;
   int rossi[]={1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36}; // array contenente tutti i numeri rossi
   int neri[]={2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35}; //array contenente tutti i numeri neri
-  int range1[]={1,2,3,4,5,6,7,8,9,10,11,12};
+  /*int range1[]={1,2,3,4,5,6,7,8,9,10,11,12};
   int range2[]={13,14,15,16,17,18,19,20,21,23,24};
   int range3[]={25,26,27,28,29,30,31,32,33,34,35,36};
   int riga1[]={1,4,7,10,13,16,19,22,25,28,31,34};
   int riga2[]={2,5,8,11,14,17,20,23,26,29,32,35};
-  int riga3[]={3,6,9,12,15,18,21,24,27,30,33,36};
-  /*struct giocate[]; array contente i valori delle singole puntate (es 10 sui rossi e 1 su un numero nella stessa partita)
-                      L: Non si dichiara così un array di struct*/
+  int riga3[]={3,6,9,12,15,18,21,24,27,30,33,36}; L: a mio avviso non servono, si possono tutti calcolare facilmente*/
+
   printf("buongiorno, bonus di primo accesso di 10000pt\n");
   do {
     printf("menu\n");
@@ -52,11 +51,11 @@ int  main() {
 }
 
 void n_puntata(punt *l){
-	int n=0,val=0;
+	int n=0,val=0, i;
 	int tipo,exit=0;
 	printf("\nQuante puntate vuoi fare?\n> ");
 	scanf("%d", &n);
-	for(int i=0;i<n;i++){
+	for(i=0;i<n;i++){
 		printf("\nPuntata n° %d\n", i+1);
 		while(exit!=1){
 			printf("\nChe tipo di puntata vuoi fare?\n");

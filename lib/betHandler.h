1bd8e701a3990acee1e6b7277dc57c7edab82e3d
Filippo <boscofil14@gmail.com>
2019-10-23 13:35:20 +0200

    merge

--- lib/betHandler.h
+++ lib/betHandler.h
@@ -7,6 +7,13 @@ bet *betPlacer(bet *, char *);
 
 bet *betPlacer(bet* head, char *tipoPunt){
 
-    if(tipoPunt == "pari" || tipoPunt == "dispari");
+    if(tipoPunt == "pari" || tipoPunt == "dispari" ||
+       tipoPunt == "manque" || tipoPunt == "passe" ||
+       tipoPunt == "rossi" || tipoPunt =="neri"){
+           printf("bella pulcini");
+       }
+    else{
+        printf("sticazzi");
+    }
 
 }

793fb5b7f4395c3ad1bfe2df265241bb48e69ec9
Luca <lukaarma@gmail.com>
2019-11-08 14:46:51 +0100

    finished registration and login module

--- lib/player.h
+++ lib/player.h
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #define SAVE_FILE "players.txt"
+#define SALDO_INIZIALE 1000
 #define MAX_USERNAME 30
 #define MAX_PASSWORD
 
@@ -19,8 +20,9 @@ player *login();                /* mostra prompt di login/registrazione, returna
 player *newPlayer();            /* registra e fa login di un nuovo utente */
 int checkUsername(char *);      /* controlla che l'username contenga soltanto
                                 [0-9, A-Z, a-z], returna 0 se non corretto */
-player *userExist(char *);      /* controlla l'esistenza del player nel file,
-                                returna null se non esiste */
+player *userExist(char *, int); /* controlla l'esistenza del player nel file,
+                                se non esiste chiede all'utente se vuole registrarsi
+                                o riprovare, flag controlla soltanto se esiste o no*/
 void serialize(player *);       /* stampa la struct fornita nel file predefinito */
 
 
@@ -33,7 +35,7 @@ player *login(){
 
     if(!(fp = fopen(SAVE_FILE, "r"))){
         while(1){
-            printf("\nIl tuo salvataggio risulta inesistente, vuoi crearne uno nuovo(N)"
+            printf("\nIl tuo salvataggio risulta inesistente, vuoi crearne uno nuovo(N) "
                 "oppure vuoi chiudere(C) il gioco? ");
             scanf(" %c%*[^]", &sceltaNewp);
             printf("\n");
@@ -53,11 +55,11 @@ player *login(){
 
     while(!player){
         do{
-            printf("\nInserisci il tuo username: ");
+            printf("\nInserisci il tuo username oppure (+) per crearne un nuovo utente: ");
             scanf(" %30s", inputName);
         }while(!checkUsername(inputName));
 
-        player = userExist(inputName);
+        player = userExist(inputName, 1);
     }
 
     /* printf("Il tuo username \x8A %s ed \x8A lungo %d caratteri!", inputName, strlen(inputName)); */
@@ -66,10 +68,24 @@ player *login(){
 }
 
 
-/* TODO: create register user function*/
 player *newPlayer(){
-    printf("Nuovo utente!!");
-    return NULL;
+    player *tmp = NULL;
+    char inputName[MAX_USERNAME + 1];
+
+
+    do{
+        printf("\nInserisci l'username che vuoi: ");
+        scanf(" %30s", inputName);
+        }while(!checkNewUser(inputName));
+
+    tmp = (player *) malloc(sizeof(player));
+
+    strcpy(tmp->username, inputName);
+    tmp->saldo = SALDO_INIZIALE;
+
+    serialize(tmp);
+
+    return tmp;
 }
 
 
@@ -92,7 +108,7 @@ int checkUsername(char *username){
 }
 
 
-player *userExist(char *username){
+player *userExist(char *username, int flag){
 
     FILE *fp;
     player *tmp = NULL;
@@ -113,16 +129,19 @@ player *userExist(char *username){
                 return tmp;
             }
             else{
-                while(1){
+                while(flag){
                     printf("\nL'utente inserito non esiste! \n"
                            "Vuoi registrarti(R) oppure inserire un altro username(U)? ");
                     scanf(" %c%*[^]", &sceltaNewp);
                     printf("\n");
                     sceltaNewp = toupper(sceltaNewp);
 
-                    if(sceltaNewp == 'R') return newPlayer();
-                    else if(sceltaNewp == 'U') return tmp;
-                    else printf("\nErrore nella scelta inserita \n");
+                    if(sceltaNewp == 'R')
+                        return newPlayer();
+                    else if(sceltaNewp == 'U')
+                        return tmp;
+                    else
+                        printf("\nErrore nella scelta inserita \n");
                 }
             }
         }
@@ -133,19 +152,34 @@ player *userExist(char *username){
 }
 
 
+int checkNewUser(char *username){
+    if(!checkUsername(username))
+        return 0;
+    if(userExist(username, 0)){
+        printf("L'username \"%s\" non è disponibile", username);
+        return 0;
+    }
+
+    return 1;
+}
+
+
 void serialize(player* out){
 
     FILE *fp;
     char *tmp;
 
-    if(tmp = malloc(sizeof(char) * sizeof(*(out))))
+    if(tmp = malloc(sizeof(char) * sizeof(*(out)))){
         sprintf(tmp, "%s;%d", out->username, out->saldo);
+        printf("%s | %s %d", tmp, out->username, out->saldo);
+    }
     else
         printf("Errore allocazione memoria per stampa a file\n\n");
 
     if(fp = fopen(SAVE_FILE, "a")){
         fprintf(fp, "%s\n", tmp);
         free(tmp);
+        fclose(fp);
     }else
         printf("Errore nell'apertura del file %s\n\n", SAVE_FILE);
 
